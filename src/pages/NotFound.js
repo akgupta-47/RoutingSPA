57ac34e177da0d582ec85d47f8ebd9074ac1af26
akgupta-47 <akshatgupta3214@gmail.com>
2021-04-28 02:16:04 +0530

    lazy loading untested yet

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import Card from "../components/UI/Card";
+
+const NotFound = () => {
+    return (
+        <Card>
+            <p className="centered">Page not found!</p>
+        </Card>
+    );
+};
+
+export default NotFound;
